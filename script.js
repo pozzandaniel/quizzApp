34eabe9b6114d797d5bbd926b938c6727057c3bc
Daniel Pozzan <pozzan.dnl@gmail.com>
2022-04-23 17:26:38 +0200

    JSON objects added

---
+++ script.js
@@ -0,0 +1,43 @@
+let questions = [
+    {
+        'question': 'Wer hat Internet entwickelt?',
+        'answer 1': 'Steve Jobs',
+        'answer 2': 'MIT und US-Verteidigungsministerium', 
+        'answer 3': 'WWF', 
+        'answer 4': 'Napoleon Bonaparte',
+        'correct': '2'
+    },
+    {
+        'question': 'Wo ist der Sitz von IBM?',
+        'answer 1': 'die Niederlanden',
+        'answer 2': 'Österreich', 
+        'answer 3': 'Irland', 
+        'answer 4': 'die Vereinigten Staaten',
+        'correct': '4'
+    },
+    {
+        'question': 'Welcher Satz gehört nicht zu Asimovs Gesetze?',
+        'answer 1': 'Ein Roboter darf kein menschliches Wesen verletzen',
+        'answer 2': 'Ein Roboter muss den ihm von einem Menschen gegebenen Befehlen gehorchen', 
+        'answer 3': 'Ein Roboter darf kein menschliches Wesen in Notfallsituationen helfen', 
+        'answer 4': 'Ein Roboter muss seine Existenz beschützen',
+        'correct': '3'
+    },
+    {
+        'question': 'Welche ist die beliebste Programmiersprache laut Redmonk-Index (2022)',
+        'answer 1': 'Javascript',
+        'answer 2': 'PHP', 
+        'answer 3': 'Swift', 
+        'answer 4': 'C/C++',
+        'correct': '1'
+    },
+    {
+        'question': 'Wann wurde das Konzept von Bitcoin erstmals vorgeschlagen?',
+        'answer 1': '1975',
+        'answer 2': '1916', 
+        'answer 3': '2018', 
+        'answer 4': '2008',
+        'correct': '4'
+    },
+
+];
