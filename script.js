a36fad2d2c79f2f39d12655b5feedd3927cb423e
Daniel Pozzan <pozzan.dnl@gmail.com>
2022-04-23 19:24:05 +0200

    button disabled/enabled

--- script.js
+++ script.js
@@ -89,6 +89,6 @@ function answer(selection){
         document.getElementById(selection).parentNode.classList.add('bg-danger');
         document.getElementById(idOfRightAnswer).parentNode.classList.add('bg-success');
     }
-    
+    document.getElementById('next-button').disabled = false;
     
 }
