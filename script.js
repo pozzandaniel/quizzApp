32f8d7042a78d2fedc9ae5cb94d4b4d98da0bcab
Daniel Pozzan <pozzan.dnl@gmail.com>
2022-04-23 18:34:25 +0200

    new version

--- script.js
+++ script.js
@@ -1,43 +1,84 @@
 let questions = [
     {
         'question': 'Wer hat Internet entwickelt?',
-        'answer 1': 'Steve Jobs',
-        'answer 2': 'MIT und US-Verteidigungsministerium', 
-        'answer 3': 'WWF', 
-        'answer 4': 'Napoleon Bonaparte',
-        'correct': '2'
+        'answer1': 'Steve Jobs',
+        'answer2': 'MIT und US-Verteidigungsministerium', 
+        'answer3': 'WWF', 
+        'answer4': 'Napoleon Bonaparte',
+        'correct': 'answer2'
     },
     {
         'question': 'Wo ist der Sitz von IBM?',
-        'answer 1': 'die Niederlanden',
-        'answer 2': 'Österreich', 
-        'answer 3': 'Irland', 
-        'answer 4': 'die Vereinigten Staaten',
+        'answer1': 'die Niederlanden',
+        'answer2': 'Österreich', 
+        'answer3': 'Irland', 
+        'answer4': 'die Vereinigten Staaten',
         'correct': '4'
     },
     {
         'question': 'Welcher Satz gehört nicht zu Asimovs Gesetze?',
-        'answer 1': 'Ein Roboter darf kein menschliches Wesen verletzen',
-        'answer 2': 'Ein Roboter muss den ihm von einem Menschen gegebenen Befehlen gehorchen', 
-        'answer 3': 'Ein Roboter darf kein menschliches Wesen in Notfallsituationen helfen', 
-        'answer 4': 'Ein Roboter muss seine Existenz beschützen',
+        'answer1': 'Ein Roboter darf kein menschliches Wesen verletzen',
+        'answer2': 'Ein Roboter muss den ihm von einem Menschen gegebenen Befehlen gehorchen', 
+        'answer3': 'Ein Roboter darf kein menschliches Wesen in Notfallsituationen helfen', 
+        'answer4': 'Ein Roboter muss seine Existenz beschützen',
         'correct': '3'
     },
     {
         'question': 'Welche ist die beliebste Programmiersprache laut Redmonk-Index (2022)',
-        'answer 1': 'Javascript',
-        'answer 2': 'PHP', 
-        'answer 3': 'Swift', 
-        'answer 4': 'C/C++',
+        'answer1': 'Javascript',
+        'answer2': 'PHP', 
+        'answer3': 'Swift', 
+        'answer4': 'C/C++',
         'correct': '1'
     },
     {
         'question': 'Wann wurde das Konzept von Bitcoin erstmals vorgeschlagen?',
-        'answer 1': '1975',
-        'answer 2': '1916', 
-        'answer 3': '2018', 
-        'answer 4': '2008',
+        'answer1': '1975',
+        'answer2': '1916', 
+        'answer3': '2018', 
+        'answer4': '2008',
         'correct': '4'
     },
+    {
+        'question': 'Welche ist die beliebste Programmiersprache laut Redmonk-Index (2022)',
+        'answer1': 'Javascript',
+        'answer2': 'PHP', 
+        'answer3': 'Swift', 
+        'answer4': 'C/C++',
+        'correct': '1'
+    }
 
 ];
+let currentQ = 0;
+
+function init() {
+   let lastQ = document.getElementById('lastQ');
+   let presentQ = document.getElementById('presentQ');
+   lastQ.innerHTML = questions.length;
+   presentQ.innerHTML = currentQ + 1;
+   showQuestion();
+}
+
+function showQuestion() {
+    let question = questions[currentQ];
+    let questionTitle = document.getElementById('questionTitle');
+    let answer1 = document.getElementById('answer1');
+    let answer2 = document.getElementById('answer2');
+    let answer3 = document.getElementById('answer3');
+    let answer4 = document.getElementById('answer4');
+    questionTitle.innerHTML = `${question['question']}`;
+    answer1.innerHTML = `${question['answer1']}`;
+    answer2.innerHTML = `${question['answer2']}`;
+    answer3.innerHTML = `${question['answer3']}`;
+    answer4.innerHTML = `${question['answer4']}`;
+    
+}
+
+function answer(selection){
+    let answer = document.getElementById(selection);
+    let right = questions[currentQ]['correct'];
+    let correctAnswer = document.getElementById(right);
+    answer.style = 'background-color: rgba(0, 0, 250, 0.1)';
+    correctAnswer.style = 'background-color: green; color: white;';
+    
+}
